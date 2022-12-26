3aa6612e652ee3bb411989a7cba86dd8b0b08231
David Reed <reedswork@proton.me>
2022-12-25 21:18:35 -0500

    add examples, use cases

---
+++ public/index.js
@@ -0,0 +1,16 @@
+/*global Typewriter*/
+
+new Typewriter(document.getElementById("def"), {
+  strings: [
+    "embedding a CORS-protected website.",
+    "simulating a NoScript environment.",
+    "simulating a noscript client",
+    "anonymizing web browsing",
+    "writing articles from a censored country",
+    "bypassing CORS restrictions",
+    "conducting research over a censored network",
+  ],
+  pauseFor: 5000,
+  autoStart: true,
+  loop: true,
+});
